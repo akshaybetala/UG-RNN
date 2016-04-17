341a2b9b26877ccb041588f1e45588cac10069c0
Akshay <akshaybetala@gmail.com>
2016-04-16 19:53:33 -0700

    Changes

--- src/Molecule.py
+++ src/Molecule.py
@@ -5,38 +5,43 @@ import parse_solubility_data
 import networkx as nx
 
 class Molecule:
-	graph = None
-	atoms = []
-	no_of_atoms = 0
-	directed_graphs = {}
-	m = None
-
-	def __init__(this,smile):
-		this.m = Chem.MolFromSmiles(smile)
-		Chem.Kekulize(this.m)
-		this.no_of_atoms = this.m.GetNumAtoms()
-		this.graph = nx.Graph()
-		for i in xrange(this.no_of_atoms):
-			this.graph.add_node(i)
-			atom = this.m.GetAtomWithIdx(i)
-			this.atoms.append(atom.GetSymbol())
+	
+	def __init__(self,smile,size):
+		self.atoms = []
+		self.m = Chem.MolFromSmiles(smile)
+		Chem.Kekulize(self.m)
+		self.no_of_atoms = self.m.GetNumAtoms()
+		self.graph = nx.Graph()
+		for i in xrange(self.no_of_atoms):
+			self.graph.add_node(i)
+			atom = self.m.GetAtomWithIdx(i)
+			self.atoms.append(atom.GetSymbol())
 			for neighbour in atom.GetNeighbors():
 				neighbour_idx = neighbour.GetIdx()
-				this.graph.add_edge(i,neighbour_idx)
+				self.graph.add_edge(i,neighbour_idx)
 
-	def get_bond_type_as_double(this,idx1,idx2):
-		bond = this.m.GetBondBetweenAtoms(i,neighbour_idx)
+		self.create_directed_graphs()
+		self.create_contextual_vectors(size)
+
+
+	def get_bond_type_as_double(self,idx1,idx2):
+		bond = self.m.GetBondBetweenAtoms(i,neighbour_idx)
 		if bond==None:
 			return 0
 		else:
 			return bond.GetBondTypeAsDouble()
 
-	def create_directed_graphs(this):
-		for idx in xrange(this.no_of_atoms):
-			path = nx.single_source_dijkstra_path(this.graph,idx)
+	def create_directed_graphs(self):
+		self.directed_graphs = {}
+		for idx in xrange(self.no_of_atoms):
+			path = nx.single_source_dijkstra_path(self.graph,idx)
 			G = nx.DiGraph()
-			for i in xrange(this.no_of_atoms):
+			for i in xrange(self.no_of_atoms):
 				temp = path[i]
 				temp.reverse()
 				G.add_path(temp)
-			this.directed_graphs[idx] = nx.topological_sort(G)
+			self.directed_graphs[idx] = nx.topological_sort(G)
+
+	def create_contextual_vectors(self,vector_length):
+		# create a three dimesnional matrix G, such that Gij is the contextual vector for ith vertex in jth DAG
+		self.contextual_vector = np.zeros((self.no_of_atoms,self.no_of_atoms,vector_length))
--- src/input_data.py
+++ src/input_data.py
@@ -8,9 +8,11 @@ import tempfile
 
 import numpy
 from six.moves import urllib
-from six.moves import xrange  # pylint: disable=redefined-builtin
+from six.moves import xrange  
 import tensorflow as tf
 import Molecule
+import parse_solubility_data
+import math
 
 class DataSet(object):
 
@@ -24,7 +26,7 @@ class DataSet(object):
     self._index_in_epoch = 0
 
   @property
-  def images(self):
+  def molecules(self):
     return self._molecules
 
   @property
@@ -39,8 +41,8 @@ class DataSet(object):
   def epochs_completed(self):
     return self._epochs_completed
 
-  def next_batch(self, batch_size, fake_data=False):
-    """Return the next `batch_size` examples from this data set."""
+  def next_batch(self, batch_size):
+    # Return the next `batch_size` examples from this data set.
     start = self._index_in_epoch
     self._index_in_epoch += batch_size
     if self._index_in_epoch > self._num_examples:
@@ -58,24 +60,27 @@ class DataSet(object):
     end = self._index_in_epoch
     return self._molecules[start:end], self._labels[start:end]
 
-def extract_molecules_from_smiles(SMILES):
+def extract_molecules_from_smiles(SMILES,contextual_vector_size):
     molecules = []
     size = len(SMILES)
     for i in xrange(size):
-      molecules.append(Molecule(SMILES[i]))
+      m = Molecule.Molecule(SMILES[i],contextual_vector_size)
+      molecules.append(m)
     return molecules
 
-def read_data_sets(dtype=tf.float32):
+def read_data_sets(contextual_vector_size):
   class DataSets(object):
     pass
   data_sets = DataSets()
 
-  SMILES, prediction_targets = load_solubility_data(path = '../data/Delaney_solubility.txt')
-  molecules = extract_molecules_from_smiles(SMILES)
+  SMILES, prediction_targets = parse_solubility_data.load_solubility_data(path = '../data/Delaney_solubility.txt')
+  molecules = extract_molecules_from_smiles(SMILES,contextual_vector_size)
   size = len(SMILES)
   
-  TRAIN_SIZE = 0.7*size
-  VALIDATION_SIZE = 0.1*size
+  TRAIN_SIZE = int(math.floor(0.7*size))
+  VALIDATION_SIZE = int(math.floor(0.1*size))
+  print(TRAIN_SIZE)
+  print(len(molecules))
   train_molecules = molecules[:TRAIN_SIZE]
   train_labels = prediction_targets[:TRAIN_SIZE]
 
@@ -85,9 +90,12 @@ def read_data_sets(dtype=tf.float32):
   test_molecules = molecules[TRAIN_SIZE+VALIDATION_SIZE:]
   test_labels = prediction_targets[TRAIN_SIZE+VALIDATION_SIZE:]
 
-  data_sets.train = DataSet(train_images, train_labels, dtype=dtype)
-  data_sets.validation = DataSet(validation_images, validation_labels,
-                                 dtype=dtype)
-  data_sets.test = DataSet(test_images, test_labels, dtype=dtype)
+  data_sets.train = DataSet(train_molecules, train_labels)
+  data_sets.validation = DataSet(validation_molecules, validation_labels)
+  data_sets.test = DataSet(test_molecules, test_labels)
 
   return data_sets
+
+data_sets = read_data_sets(10)
+molecules,labels = data_sets.train.next_batch(2)
+print(molecules[0].contextual_vector)
